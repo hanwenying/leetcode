a4092cb4b1a8f1024cb61dee154c20066bc4b885
hanwenying <hanwenying@baidu.com>
2016-08-02 17:42:51 +0800

    parameter reference

---
+++ moveZeroes.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def __init__(self, nums):
+        self.nums = nums
+
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        zero_pos = 0
+        nozero_pos = -1
+        for i in range(len(nums)):
+            if nums[i]:
+                nozero_pos += 1
+                nums[nozero_pos] = nums[zero_pos]
+            zero_pos += 1
+
+        for i in range(nozero_pos + 1, len(nums)):
+            nums[i] = 0
+
+if __name__ == '__main__':
+    nums = [0,1,0,3,12]
+    solution = Solution(nums)
+    solution.moveZeroes(nums)
+    print nums
